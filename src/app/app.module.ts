943cd8d956aa300614a73d0879dedbc86bbb08e4
Boglarka <boglarkaferencsik05@gmail.com>
2024-01-14 19:54:14 +0100

    kész

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TreeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
